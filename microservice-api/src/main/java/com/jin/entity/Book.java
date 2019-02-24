3e9fc2d9b330f0d86649081f7aa4c13bab99946f
jinshuaishuai <jin2111843364@163.com>
2019-02-24 23:35:31 +0800

    更新Book类,使其实现Serializable接口

--- microservice-api/src/main/java/com/jin/entity/Book.java
+++ microservice-api/src/main/java/com/jin/entity/Book.java
@@ -1,12 +1,19 @@
 package com.jin.entity;
 
+import java.io.Serializable;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
-public class Book {
+public class Book implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private Long id;
 	
 	private String bookName;
