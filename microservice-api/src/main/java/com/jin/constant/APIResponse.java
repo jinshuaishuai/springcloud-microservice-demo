c403c2d351defb6e04e1cc66c31cded1b9112c42
jinshuaishuai <jin2111843364@163.com>
2019-02-27 00:32:43 +0800

    添加全局响应处理对象

---
+++ microservice-api/src/main/java/com/jin/constant/APIResponse.java
@@ -0,0 +1,19 @@
+package com.jin.constant;
+
+import java.io.Serializable;
+
+import lombok.Data;
+@Data
+public class APIResponse<T> implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String msg;
+	
+	private String code;
+	
+	private T result;
+}
