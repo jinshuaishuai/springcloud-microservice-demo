225e187b85ae971b1688d494bf43ba0159f76f88
jinshuaishuai <jin2111843364@163.com>
2019-02-27 00:46:48 +0800

    构建消费者工程

---
+++ microservice/microservice-book-consumer/src/main/java/com/jin/ConsumerSpringApplication.java
@@ -0,0 +1,11 @@
+package com.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerSpringApplication.class, args);
+	}
+}
