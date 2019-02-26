f4c690cb7f236093c7d4314dcf6dd72a810018e4
jinshuaishuai <jin2111843364@163.com>
2019-02-27 00:49:07 +0800

    将分支develope上的代码合并到master上

--- microservice/microservice-book-consumer/src/main/java/com/jin/ConsumerSpringApplication.java
+++ microservice/microservice-book-consumer/src/main/java/com/jin/ConsumerSpringApplication.java
@@ -3,6 +3,12 @@ package com.jin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 消费者启动类
+ * @author shuai.jin
+ * @datetime 2019年2月27日  00:48:28
+ *
+ */
 @SpringBootApplication
 public class ConsumerSpringApplication {
 	public static void main(String[] args) {
