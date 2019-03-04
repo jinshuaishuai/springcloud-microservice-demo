76c7ed5a2d95e70d73610eb9197b903ed466e38f
jinshuaishuai <jin2111843364@163.com>
2019-03-05 00:36:19 +0800

    代码结构优化

--- microservice-provider/src/main/java/com/jin/config/AppConfig.java
+++ microservice-provider/src/main/java/com/jin/config/AppConfig.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
 @Component
 @PropertySource(encoding = "utf-8",value = "classpath:property.properties")
-
+@Data
 public class AppConfig {
 	//Mysql配置
 	@Value("${mysql.driver.name}")
@@ -23,42 +25,7 @@ public class AppConfig {
 	private String host;
 	@Value("${redis.port}")
 	private int port;
-	public String getDriverName() {
-		return driverName;
-	}
-	public void setDriverName(String driverName) {
-		this.driverName = driverName;
-	}
-	public String getJdbcUrl() {
-		return jdbcUrl;
-	}
-	public void setJdbcUrl(String jdbcUrl) {
-		this.jdbcUrl = jdbcUrl;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPwd() {
-		return pwd;
-	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-	public String getHost() {
-		return host;
-	}
-	public void setHost(String host) {
-		this.host = host;
-	}
-	public int getPort() {
-		return port;
-	}
-	public void setPort(int port) {
-		this.port = port;
-	}
+	
 	
 	
 }
