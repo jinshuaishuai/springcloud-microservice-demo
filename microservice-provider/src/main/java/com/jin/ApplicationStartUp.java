7fdede976c078c327efc8ae9b77a5677ee4db760
jinshuaishuai <jin2111843364@163.com>
2019-03-04 23:52:12 +0800

    成功搭建Eureka服务注册中心,并成功发现注册进来的微服务

--- microservice-provider/src/main/java/com/jin/ApplicationStartUp.java
+++ microservice-provider/src/main/java/com/jin/ApplicationStartUp.java
@@ -2,12 +2,14 @@ package com.jin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * 
  * @author shuai.jin
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationStartUp {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationStartUp.class, args);
